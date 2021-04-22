32b508d43840fa5f70b97ad5e70f9ac066efdecd
Mark Pollack <mpollack@vmware.com>
2021-04-22 14:47:09 -0400

    initial

---
+++ src/main/java/com/example/coolproject/CoolprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.coolproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coolproject/CoolprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
